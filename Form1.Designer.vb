fba1942ce4bcdae9759dc2fcc130f115b38ac258
Bishal <adbishal569@gmail.com>
2023-05-13 11:20:02 +0545

    First change in Login Form

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,8 +22,8 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
         lbl_PName = New Label()
-        lbl_login = New Label()
         lbl_UserName = New Label()
         lbl_password = New Label()
         btn_login = New Button()
@@ -34,45 +34,36 @@ Partial Class Form1
         ' lbl_PName
         ' 
         lbl_PName.AutoSize = True
+        lbl_PName.BackColor = Color.Transparent
         lbl_PName.Font = New Font("Century Gothic", 28.2F, FontStyle.Bold, GraphicsUnit.Point)
-        lbl_PName.ForeColor = Color.Chocolate
-        lbl_PName.Location = New Point(208, 82)
+        lbl_PName.ForeColor = Color.Snow
+        lbl_PName.Location = New Point(261, 57)
         lbl_PName.Name = "lbl_PName"
-        lbl_PName.Size = New Size(630, 56)
+        lbl_PName.Size = New Size(457, 112)
         lbl_PName.TabIndex = 0
-        lbl_PName.Text = "Cafe Management System"
-        ' 
-        ' lbl_login
-        ' 
-        lbl_login.AutoSize = True
-        lbl_login.Font = New Font("Century Gothic", 22.2F, FontStyle.Bold Or FontStyle.Underline, GraphicsUnit.Point)
-        lbl_login.ForeColor = Color.Chocolate
-        lbl_login.Location = New Point(443, 186)
-        lbl_login.Name = "lbl_login"
-        lbl_login.Size = New Size(114, 44)
-        lbl_login.TabIndex = 1
-        lbl_login.Text = "Login"
+        lbl_PName.Text = "Cafe Management" & vbCrLf & "          System"
         ' 
         ' lbl_UserName
         ' 
         lbl_UserName.AutoSize = True
-        lbl_UserName.BackColor = Color.Snow
-        lbl_UserName.Font = New Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
-        lbl_UserName.ForeColor = Color.Chocolate
-        lbl_UserName.Location = New Point(283, 269)
+        lbl_UserName.BackColor = Color.Transparent
+        lbl_UserName.Font = New Font("Century Gothic", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        lbl_UserName.ForeColor = Color.Snow
+        lbl_UserName.Location = New Point(178, 273)
         lbl_UserName.Name = "lbl_UserName"
-        lbl_UserName.Size = New Size(169, 34)
+        lbl_UserName.Size = New Size(225, 44)
         lbl_UserName.TabIndex = 2
         lbl_UserName.Text = "User Name "
         ' 
         ' lbl_password
         ' 
         lbl_password.AutoSize = True
-        lbl_password.Font = New Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
-        lbl_password.ForeColor = Color.Chocolate
-        lbl_password.Location = New Point(283, 347)
+        lbl_password.BackColor = Color.Transparent
+        lbl_password.Font = New Font("Century Gothic", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        lbl_password.ForeColor = Color.Snow
+        lbl_password.Location = New Point(196, 366)
         lbl_password.Name = "lbl_password"
-        lbl_password.Size = New Size(142, 34)
+        lbl_password.Size = New Size(186, 44)
         lbl_password.TabIndex = 3
         lbl_password.Text = "Password"
         ' 
@@ -81,7 +72,7 @@ Partial Class Form1
         btn_login.BackColor = Color.Chocolate
         btn_login.Font = New Font("Century Gothic", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
         btn_login.ForeColor = Color.Snow
-        btn_login.Location = New Point(443, 420)
+        btn_login.Location = New Point(392, 490)
         btn_login.Name = "btn_login"
         btn_login.Size = New Size(184, 66)
         btn_login.TabIndex = 4
@@ -92,7 +83,7 @@ Partial Class Form1
         ' 
         txt_username.Font = New Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
         txt_username.ForeColor = Color.Chocolate
-        txt_username.Location = New Point(458, 266)
+        txt_username.Location = New Point(409, 273)
         txt_username.Name = "txt_username"
         txt_username.Size = New Size(224, 41)
         txt_username.TabIndex = 5
@@ -101,7 +92,7 @@ Partial Class Form1
         ' 
         txt_password.Font = New Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
         txt_password.ForeColor = Color.Chocolate
-        txt_password.Location = New Point(458, 344)
+        txt_password.Location = New Point(409, 369)
         txt_password.Name = "txt_password"
         txt_password.Size = New Size(224, 41)
         txt_password.TabIndex = 6
@@ -111,13 +102,14 @@ Partial Class Form1
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.Snow
-        ClientSize = New Size(1009, 557)
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Zoom
+        ClientSize = New Size(951, 590)
         Controls.Add(txt_password)
         Controls.Add(txt_username)
         Controls.Add(btn_login)
         Controls.Add(lbl_password)
         Controls.Add(lbl_UserName)
-        Controls.Add(lbl_login)
         Controls.Add(lbl_PName)
         Name = "Form1"
         Text = "Form1"
@@ -126,7 +118,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents lbl_PName As Label
-    Friend WithEvents lbl_login As Label
     Friend WithEvents lbl_UserName As Label
     Friend WithEvents lbl_password As Label
     Friend WithEvents btn_login As Button
